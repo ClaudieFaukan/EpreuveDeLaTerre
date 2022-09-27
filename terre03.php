88236fa46c07b06165ca22ef29742798a8265dd2
Pierre-Alain <pierrealain.petrel@viacesi.fr>
2022-09-27 18:02:55 +0200

    ajout de verification

--- terre03.php
+++ terre03.php
@@ -2,9 +2,16 @@
 
 $alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-$argument = $argv[1];
+//check si argument passer et si c'est un string
+if (count($argv) <= 1 || !is_string($argv[1]) || is_numeric($argv[1]) || strlen($argv[1]) > 1) {
+    echo ("Argument non valide , Saisir une lettre \n");
+    die();
+}
 
-$index_de_argument = array_keys($alphabet, strtolower($argument));
+$argument = strval($argument);
+$argument = strtolower($argument);
+
+$index_de_argument = array_keys($alphabet, $argument);
 
 for ($i = $index_de_argument[0]; $i < count($alphabet); $i++) {
     echo ($alphabet[$i]);
