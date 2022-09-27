220d5050c3be1f7fc741895683a06bd56579f570
Pierre-Alain <pierrealain.petrel@viacesi.fr>
2022-09-27 18:28:02 +0200

    add terre05

--- terre05.php
+++ terre05.php
@@ -1,1 +1,20 @@
 <?php
+
+//verifie si argument passer ou non, etablie par defaut une valeur null
+if (count($argv) <= 1 || is_numeric($argv[1]) || !is_string($argv[1])) {
+    echo "Veuillez saisir, une entree valide de type 'string' exemple: \"Coucou les amis\" \n";
+    die();
+}
+$argument = "";
+
+if (count($argv) >= 2) {
+
+    for ($i = 1; $i < count($argv); $i++) {
+
+        $argument .= " " . strval($argv[$i]);
+    }
+} else {
+
+    $argument = $argv[1];
+}
+echo strrev($argument);
