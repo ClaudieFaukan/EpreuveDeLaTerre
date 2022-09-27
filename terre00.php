d4face866aae84cef3924485672a55813fc6a8f3
Pierre-Alain <pierrealain.petrel@viacesi.fr>
2022-09-27 15:55:51 +0200

    first commit

---
+++ terre00.php
@@ -0,0 +1,7 @@
+<?php
+
+$alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+foreach ($alphabet as $lettre) {
+    echo ("$lettre \n");
+}
